165530ee98055007669a6546678f107e26084a25
habtiebaye <habtie1080@gmail.com>
2022-11-29 12:38:19 +0000

    Task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,6 +2,6 @@
 
 for letter in range(97, 123):
 
-    if chr(letter) !='q' and chr(letter) !='e':
+    if chr(letter) != 'q' and chr(letter) != 'e':
 
         print("{}".format(chr(letter)), end="")
